7023c8d9a142ae340da9b4cb5bc81ce49ee99060
dshtjefni <dnld.sht@gmail.com>
2021-03-28 12:30:59 +0200

    vite example

---
+++ examples/vite/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Story>
+      <div
+        class="slide"
+        v-for="i in 10"
+        :key="i"
+        :style="{ background: colors[i % colors.length] }"
+      >
+        Story {{ i }}
+      </div>
+    </Story>
+  </div>
+</template>
+
+<script>
+// import Story from "vue-insta-stories/src/components/Story";
+import Story from "../../../src/Story.vue";
+export default {
+  name: "App",
+  components: { Story },
+
+  data: () => ({
+    colors: ["#DAF7A6", "#FFC300", "#FF5733"],
+  }),
+};
+</script>
+<style scoped>
+.slide {
+  /* Take the rest of the page */
+  flex-grow: 1;
+
+  /* Center align */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
